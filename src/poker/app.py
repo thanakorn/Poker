af77364343720b9ad0125da0125edc280a12ec62
Thanakorn <stylus_l@hotmail.com>
2013-01-11 21:10:32 +0700

    Add more test case

--- src/poker/app.py
+++ src/poker/app.py
@@ -16,7 +16,13 @@ def test():
     sf = "6C 7C 8C 9C TC".split()
     fk = "9D 9H 9S 9C 7D".split()
     fh = "TD TC TH 7D 7C".split()
+    
     assert poker([sf,fk,fh]) == sf
+    assert poker([fk,fh]) == fk
+    assert poker([fh,fh]) == fh
+    assert poker([sf]) == sf
+    assert poker([sf] + 99*[fk]) == sf
+    
     print "Test pass"
     
     
